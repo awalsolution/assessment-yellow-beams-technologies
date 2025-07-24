d74ce4640f5ca8c0d2d51db3ebecf37641acfadf
IQBAL HASSAN <iqbal.hassan2584@gmail.com>
2025-07-24 17:54:25 +0500

    Expose password field in User model
    Removed 'select: false' from the password field in the User schema, making it selectable in queries. This may affect security and should be reviewed to ensure passwords are handled safely.

--- src/models/user.ts
+++ src/models/user.ts
@@ -16,7 +16,6 @@ const UserSchema: Schema = new Schema(
       type: String,
       required: [true, 'Password is required'],
       minlength: [6, 'Password must be at least 6 characters'],
-      select: false,
     },
   },
   {
